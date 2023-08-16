d7eb923216ace662242e9edf681da3749a99d915
Josias <kologojosias@gmail.com>
2023-08-16 11:59:31 +0100

    added new app

--- src/App.js
+++ src/App.js
@@ -114,11 +114,7 @@ const App = () => {
                   value: "#ffffff",
                 },
                 links: {
-                  color: "#ffffff",
-                  distance: 150,
                   enable: true,
-                  opacity: 0.5,
-                  width: 1,
                 },
                 collisions: {
                   enable: true,
